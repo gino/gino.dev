4d025752f0405d5e29de4592dadbf0920894a657
Gino <admin@gino.dev>
2022-11-21 11:06:49 +0100

    Designed a few bits of the hero banner on the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,62 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import clsx from "clsx";
+import { PropsWithChildren } from "react";
+import { TypographyLogo } from "../components/ui/TypographyLogo";
 
-const Home: NextPage = () => {
+function Highlight({ children }: PropsWithChildren) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
+    <span className="font-semibold text-white transition duration-300 ease-in-out border-b-2 cursor-pointer border-white/10 hover:border-blue-600">
+      {children}
+    </span>
+  );
 }
 
-export default Home
+function Index() {
+  return (
+    <div className="max-w-4xl py-16 mx-auto space-y-16">
+      <header>
+        <div>
+          <TypographyLogo className="w-12" />
+        </div>
+      </header>
+
+      <div>
+        <div className="max-w-xl mb-16 leading-relaxed text-white/60">
+          <div className="w-16 h-16 mb-8 overflow-hidden rounded-full bg-white/10">
+            <img
+              src="https://64.media.tumblr.com/9548df747fb7c87e7200e8d93fc9b6c0/tumblr_odj0xsEJ0E1vai64fo1_1280.jpg"
+              className="object-cover object-center w-full h-full"
+            />
+          </div>
+          <div className="space-y-4">
+            <div>
+              I'm a full-stack <Highlight>software engineer</Highlight> from The
+              Netherlands.
+            </div>
+            <div className="text-white/40">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Repudiandae facere dicta maxime doloribus labore alias nobis
+              consectetur assumenda expedita, aspernatur quo ut ullam
+              perferendis impedit! Laboriosam fugit, soluta deleniti.
+            </div>
+          </div>
+        </div>
+        <div className="inline-flex flex-col space-y-4">
+          <div className="px-5 py-3 text-sm font-medium tracking-wide rounded-lg bg-white/5 text-white/60">
+            Available for{" "}
+            <span className="font-semibold text-white">commissions</span>
+          </div>
+          <button
+            className={clsx(
+              "font-medium px-16 py-3 bg-clip-padding bg-white/90 hover:bg-white focus:outline-none rounded-xl text-black border-4 border-white/30 transition duration-200 ease-in-out relative",
+              "before:absolute before:[background-image:conic-gradient(from_0deg,var(--tw-gradient-stops))] before:to-blue-600 before:from-purple-700 before:opacity-30 before:rounded-xl before:blur-xl before:inset-0 before:-z-[1] hover:before:opacity-60 before:transition before:duration-700 before:ease-in-out before:will-change-[filter]"
+            )}
+          >
+            Get in touch
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Index;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
